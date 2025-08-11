c90731c5b3cc6ffb467d977f1360a10f75abb62e
HamidSafdari7 <137833289+Ehsan1385@users.noreply.github.com>
2025-08-11 19:02:41 +0430

    add my project

---
+++ products.php
@@ -0,0 +1,83 @@
+<?php include 'header.php' ?>
+<main class="w-full items-start flex gap-[24px] bg-gray-100 w-full max-w-[1440px] py-[80px] px-[16px] mx-auto">
+        <div
+            class="w-[350px] bg-white flex flex-col gap-[14px] py-[16px] px-[8px] border border-gray-300 rounded-lg shadow-sm">
+            <p class="text-black font-bold text-[18px]">
+                محصولات مشابه
+            </p>
+            <div>
+                <div class="flex justify-between items-center mx-[16px] border-b border-gray-200 gap-[14px] py-[18px]">
+                    <img class="w-[48px] h-[48px] object-cover" src="assets/images/Image.jpg" />
+                    <div class="flex flex-col gap-[8px]">
+                        <p class="text-gray-500 font-medium text-[14px]">
+                            دوربین دیجیتال اکسون مدل
+                        </p>
+                        <p class="text-gray-500 font-medium">
+                            AX6062
+                        </p>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center mx-[16px] border-b border-gray-200 gap-[14px] py-[18px]">
+                    <img class="w-[48px] h-[48px] object-cover" src="assets/images/Image.jpg" />
+                    <div class="flex flex-col gap-[8px]">
+                        <p class="text-gray-500 font-medium text-[14px]">
+                            دوربین دیجیتال اکسون مدل
+                        </p>
+                        <p class="text-gray-500 font-medium">
+                            AX6062
+                        </p>
+                    </div>
+                </div>
+                <div class="flex justify-between items-center mx-[16px] gap-[14px] py-[18px]">
+                    <img class="w-[48px] h-[48px] object-cover" src="assets/images/Image.jpg" />
+                    <div class="flex flex-col gap-[8px]">
+                        <p class="text-gray-500 font-medium text-[14px]">
+                            دوربین دیجیتال اکسون مدل
+                        </p>
+                        <p class="text-gray-500 font-medium">
+                            AX6062
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        
+        <div class="w-full flex flex-col gap-[16px]">
+            <img class="w-full h-[500px] rounded-lg" src="assets/images/Image.jpg" />
+            <div class="flex justify-between">
+                <p class="text-gray-600 font-bold">دوربین دیجیتال اکسون مدلAX6062</p>
+                <div class="flex items-center gap-[16px]">
+                <span class="rounded-[8px] bg-red-500 px-[12px] py-[8px] text-white">4%</span>
+                <p class="text-gray-500 line-through font-bold">2,899,000 </p>
+                <p class="text-black font-bold">2,799,000 <span class="text-gray-600 font-medium">تومان</span>
+                </p>
+            </div>
+            </div>
+            <div class="px-[16px]">
+                <p class="text-gray-500">لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها
+                    و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+                    کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و
+                    آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه
+                    ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                    تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی
+                    دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                    لورم ایپسوم متن ساختگی با تولید سادگی نامفهوم از صنعت چاپ و با استفاده از طراحان گرافیک است. چاپگرها
+                    و متون بلکه روزنامه و مجله در ستون و سطرآنچنان که لازم است و برای شرایط فعلی تکنولوژی مورد نیاز و
+                    کاربردهای متنوع با هدف بهبود ابزارهای کاربردی می باشد. کتابهای زیادی در شصت و سه درصد گذشته، حال و
+                    آینده شناخت فراوان جامعه و متخصصان را می طلبد تا با نرم افزارها شناخت بیشتری را برای طراحان رایانه
+                    ای علی الخصوص طراحان خلاقی و فرهنگ پیشرو در زبان فارسی ایجاد کرد. در این صورت می توان امید داشت که
+                    تمام و دشواری موجود در ارائه راهکارها و شرایط سخت تایپ به پایان رسد وزمان مورد نیاز شامل حروفچینی
+                    دستاوردهای اصلی و جوابگوی سوالات پیوسته اهل دنیای موجود طراحی اساسا مورد استفاده قرار گیرد.
+                </p>
+                <br>
+                <button class="rounded-full bg-blue-200 px-[12px] py-[8px]">افزودن به سبد</button>
+                <br><br>
+                <p class="font-bold">ویژگی ها</p>
+                <br>
+                <p class="text-gray-500">نوع حسگر: <span class="font-bold">CMOS</span></p>
+                <br>
+                <p class="text-gray-500">قطع حسگر: <span class="font-bold">(کراپ فریم)test/test</span></p>
+            </div>
+        </div>
+    </main>
+<?php include 'footer.php' ?>
