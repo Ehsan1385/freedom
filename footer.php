8c493421a0df047b3a1457bb99da19a55011d913
Ehsan Safdari <137833289+Ehsan1385@users.noreply.github.com>
2025-08-11 19:06:27 +0430

    add project

---
+++ footer.php
@@ -0,0 +1,18 @@
+<footer>
+        <div class="w-full max-w-[1440px] mx-auto flex justify-between items-center px-[16px]">
+            <div class="flex items-center gap-[16px]">
+                <img class="w-[60px] h-[60px] object-cover" src="assets/images/HodeCode 2 - logo - shadowed.png" alt="icon">
+            </div>
+            <div>
+                <p class="font-bold flex text-center">کلیه حقوق این سایت برای پارت محفوظ میباشد</p>
+            </div>
+            <div class="flex items-center gap-[16px]">
+                <img class="w-[25px] h-[25px] object-cover" src="assets/images/facebook.png" alt="Icon">
+                <img class="w-[38px] h-[38px] object-cover" src="assets/images/linkdin.png" alt="Icon">
+                <img class="w-[25px] h-[25px] object-cover" src="assets/images/twitter.png" alt="Icon">
+            </div>
+        </div>
+    </footer>
+</body>
+
+</html>
---
+++ header.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en" dir="rtl">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="assets/images/HodeCode 2 - logo - shadowed.png" type="image/x-icon">
+    <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    <title>Hod.Code2</title>
+</head>
+
+<body class="">
+    <header class="w-full h-[60px] bg-white border-b border-gray-300 shadow-lg">
+        <div class="w-full max-w-[1440px] mx-auto flex justify-between items-center px-[16px]">
+
+            <div class="flex items-center gap-[16px]">
+                <img class="w-[60px] h-[60px] object-cover" src="assets/images/HodeCode 2 - logo - shadowed.png" alt="icon">
+                <a>
+                    خانه
+                </a>
+                <a>
+                    محصولات
+                </a>
+            </div>
+            <div class="flex items-center gap-[16px]">
+                <a>
+                    ارتباط با ما
+                </a>
+                <img class="w-[25px] h-[25px]" src="assets/images/shopingCard.png" alt="Icon">
+            </div>
+        </div>
+    </header>
